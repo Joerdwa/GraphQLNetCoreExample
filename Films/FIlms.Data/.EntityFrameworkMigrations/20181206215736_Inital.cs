c35a62efe7bf2f7894239711ebf8b94b7a2f779d
Joe Scott <joe.scott.555@gmail.com>
2018-12-06 22:02:50 +0000

    Add Initial Migration
    Added Initial Migration

---
+++ Films/FIlms.Data/.EntityFrameworkMigrations/20181206215736_Inital.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Films.Data.EntityFrameworkMigrations
+{
+    public partial class Inital : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Films",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Films", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Films");
+        }
+    }
+}
